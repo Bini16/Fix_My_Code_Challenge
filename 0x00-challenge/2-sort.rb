340def56f8f60dab134522cd8e1e46e9e6137e21
Bini16 <abrahambiniyam6@gmail.com>
2023-10-29 14:36:13 -0700

    fix my code updated project tasks

---
+++ 0x00-challenge/2-sort.rb
@@ -0,0 +1,29 @@
+###
+#  Sort integer arguments (ascending) 
+###
+
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+
+    # insert result at the right position
+    is_inserted = false
+
+    # Iterate through the result array to find the correct position
+    result.each_with_index do |val, i|
+        if i_arg < val
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+
+    # If it wasn't inserted, add it to the end
+    result << i_arg unless is_inserted
+end
+
+puts result
